18be0ccea869ce3be1904233fbe1ef72781508b3
TabTechy <angeltabitha27@gmail.com>
2025-04-06 21:47:28 +0530

    Added full working project!
    Files include: Homepage, Donor form, AI Freshness Detection, Database updating, Google Map pins of food availability.

---
+++ home.js
@@ -0,0 +1,11 @@
+// // Select the Button
+// const donateButton = document.querySelector('.btn-primary');
+
+// // Add a Click Event (What happens when clicked)
+// donateButton.addEventListener('click', function() {
+//     alert('Thank you for your interest in donating food! 😊');
+// });
+// // Change Background Color on Click
+// donateButton.addEventListener('click', function() {
+//     document.body.style.backgroundColor = "#f5e6ca"; // Light Beige
+// });
